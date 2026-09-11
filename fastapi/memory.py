6d15dc674fc9c6eeab2c44abb6c2324b44760f33
jonathan.laptop@mindbodyengineer.com <jonathan.laptop@mindbodyengineer.com>
2026-09-11 13:47:22 -0600

    new api is so simple!

--- fastapi/memory.py
+++ fastapi/memory.py
@@ -80,7 +80,7 @@ def ensure_memory_collection() -> None:
             return
         client.collections.create(
             "MemoryArtifact",
-            vectorizer_config=Configure.Vectorizer.none(),
+            vector_config=Configure.Vectorizer.none(),
         )
     finally:
         client.close()
