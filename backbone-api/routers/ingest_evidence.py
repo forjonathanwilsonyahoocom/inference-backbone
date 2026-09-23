3bda5f8fa99910585d10f376781f53d4fba0d42d
jonathan.laptop@mindbodyengineer.com <jonathan.laptop@mindbodyengineer.com>
2026-09-23 15:14:58 -0600

    missed import

--- backbone-api/routers/ingest_evidence.py
+++ backbone-api/routers/ingest_evidence.py
@@ -8,7 +8,7 @@ from projections.graphdb.evidence import write_evidence_to_graphdb
 from util.chunker import chunk_text
 from typing import List, Dict
 from util.embedding_provider import OllamaEmbeddingProvider
-from util.file_persistence import write_evidence_to_file
+from util.file_persistence import write_to_file
 from util.identity import context_based_id
 from clients.weaviate import get_weaviate_client, ensure_weaviate_collection
 
